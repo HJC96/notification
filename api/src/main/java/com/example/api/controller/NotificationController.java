72dafbfe8b0d9fe56514e560866b715ce63824b5
HJC96 <gkswlcjs2@naver.com>
2025-04-26 22:15:01 +0900

    feat: swagger 추가

--- api/src/main/java/com/example/api/controller/NotificationController.java
+++ api/src/main/java/com/example/api/controller/NotificationController.java
@@ -2,11 +2,14 @@ package com.example.api.controller;
 
 import com.example.api.dto.NotificationRequest;
 import com.example.api.service.NotificationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Notification API", description = "알림 관련 API")
 @RestController
 @RequestMapping("/notifications")
 @RequiredArgsConstructor
@@ -17,6 +20,7 @@ public class NotificationController {
     /**
      * 알림 생성 요청을 처리하는 엔드포인트
      */
+    @Operation(summary = "알림 전송", description = "알림을 특정 사용자에게 전송합니다.")
     @PostMapping
     public ResponseEntity<Void> sendNotification(@RequestBody NotificationRequest request) {
         notificationService.sendNotification(request);
--- api/src/main/java/com/example/api/dto/NotificationRequest.java
+++ api/src/main/java/com/example/api/dto/NotificationRequest.java
@@ -1,13 +1,17 @@
 package com.example.api.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class NotificationRequest {
+    @Schema(example = "1", description = "사용자 ID")
     private Long userId;
+    @Schema(example = "알림 제목", description = "알림 제목")
     private String title;
+    @Schema(example = "알림 내용", description = "알림 내용")
     private String body;
 
     public NotificationRequest(Long userId, String title, String body) {

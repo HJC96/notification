01777cb93acfbb0314b4a58be86b5f00108090af
HJC96 <gkswlcjs2@naver.com>
2025-04-30 00:27:54 +0900

    feat: 제3자 서비스 응답 실패시 retry 기능 추가. 1. notification-event 토픽에서 첫 소비 실패 2. Spring이 자동으로 notification-event-retry 토픽으로 보내서 3. retry-consumer들이 notification-event-retry 를 계속 소비하면서 재시도 4. 여기서도 실패하면 → notification-event-dlq 로 최종 이동

--- consumer/src/main/java/com/example/consumer/ConsumerApplication.java
+++ consumer/src/main/java/com/example/consumer/ConsumerApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.EnableKafka;
 
-@EnableKafka // KafkaListener를 사용하려면 꼭 필요
 @SpringBootApplication(scanBasePackages = "com.example.consumer")
 public class ConsumerApplication {
     public static void main(String[] args) {
--- consumer/src/main/java/com/example/consumer/config/KafkaConsumerConfig.java
+++ consumer/src/main/java/com/example/consumer/config/KafkaConsumerConfig.java
@@ -1,4 +1,48 @@
 package com.example.consumer.config;
 
+import com.example.core.event.NotificationEvent;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@EnableKafka
+@Configuration
 public class KafkaConsumerConfig {
+
+    @Bean
+    public ProducerFactory<String, NotificationEvent> notificationEventProducerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Bean
+    public KafkaTemplate<String, NotificationEvent> notificationEventKafkaTemplate(
+            ProducerFactory<String, NotificationEvent> producerFactory) {
+        return new KafkaTemplate<>(producerFactory);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory(
+            ConsumerFactory<String, Object> consumerFactory) {
+
+        ConcurrentKafkaListenerContainerFactory<String, Object> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory);
+
+        return factory;
+    }
 }
